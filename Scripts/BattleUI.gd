186b83c470d4b8e9a3ac381de22238ad706f0542
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-10-05 15:54:24 -0400

    Initialize health bars upon battle scene start

--- Scripts/BattleUI.gd
+++ Scripts/BattleUI.gd
@@ -1,8 +1,18 @@
 extends MarginContainer
 
-func handle_player_health_changed(health: int):
-	print(health)
-	$Rows/Columns/PlayerHealthBar.value = health
+onready var player_health_bar = $Rows/Columns/PlayerHealthBar
+onready var enemy_health_bar = $Rows/Columns/EnemyHealthBar
 
-func handle_enemy_health_changed(health: int):
-	$Rows/Columns/EnemyHealthBar.value = health
+func initialize_player_health(max_health: int):
+	player_health_bar.max_value = max_health
+	player_health_bar.value = max_health
+
+func initialize_enemy_health(max_health: int):
+	enemy_health_bar.max_value = max_health
+	enemy_health_bar.value = max_health
+
+func handle_player_health_changed(new_health: int):
+	player_health_bar.value = new_health
+
+func handle_enemy_health_changed(new_health: int):
+	enemy_health_bar.value = new_health
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -3,6 +3,11 @@ extends Node2D
 onready var player = $Player
 onready var enemy = $Enemy
 
+onready var ui = $BattleUI
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	ui.initialize_player_health(PlayerVariables.maxHealth)
+	ui.initialize_enemy_health(enemy.health)
+
 	enemy.set_target(player)
