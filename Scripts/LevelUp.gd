07e426636ab92bfbac606f04420c0fc89a4a7430
Dan Kelch <dan.kelch@atomicobject.com>
2019-10-05 16:00:54 -0400

    Update the level up screen

--- Scripts/LevelUp.gd
+++ Scripts/LevelUp.gd
@@ -8,14 +8,17 @@ var max_level = 10
 
 var starting_health = 3
 var starting_strength = 5
+var starting_speed = 1
 
-var health = 3
-var strength = 5
+var health = starting_health
+var strength = starting_strength
+var speed = starting_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Rows/StrengthLevelUpField.set_value(strength)
 	$Rows/HealthLevelUpField.set_value(health)
+	$Rows/SpeedLevelUpField.set_value(speed)
 	
 	$Rows/HBoxContainer/RemainingPointsValue.text = str(points_remaining)
 		
@@ -34,21 +37,31 @@ func increment(startingLevel, currentLevel):
 		$Rows/HBoxContainer/RemainingPointsValue.text = str(points_remaining)
 	return currentLevel
 
-func _on_StrengthLevelUpField_decrease_pressed():
-	strength = decrement(starting_strength, strength)
-	$Rows/StrengthLevelUpField.set_value(strength)
-
+# INCREMENT
 
 func _on_StrengthLevelUpField_increase_pressed():
 	strength = increment(starting_strength, strength)
 	$Rows/StrengthLevelUpField.set_value(strength)
 
+func _on_HealthLevelUpField_increase_pressed():
+	health = increment(starting_health, health)
+	$Rows/HealthLevelUpField.set_value(health)
+
+func _on_SpeedLevelUpField_increase_pressed():
+	speed = increment(starting_speed, speed)
+	$Rows/SpeedLevelUpField.set_value(speed)
+
+
+# DECREMENT
+	
+func _on_StrengthLevelUpField_decrease_pressed():
+	strength = decrement(starting_strength, strength)
+	$Rows/StrengthLevelUpField.set_value(strength)
 
 func _on_HealthLevelUpField_decrease_pressed():
 	health = decrement(starting_health, health)
 	$Rows/HealthLevelUpField.set_value(health)
 
-
-func _on_HealthLevelUpField_increase_pressed():
-	health = increment(starting_health, health)
-	$Rows/HealthLevelUpField.set_value(health)
+func _on_SpeedLevelUpField_decrease_pressed():
+	speed = decrement(starting_speed, speed)
+	$Rows/SpeedLevelUpField.set_value(speed)
