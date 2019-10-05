baf31fb592bbd56893bef199b1bd19736b2cca5e
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-10-05 18:25:16 -0400

    Add number to level up screen to indicate current level

--- Scripts/LevelUpField.gd
+++ Scripts/LevelUpField.gd
@@ -6,7 +6,7 @@ signal decrease_pressed()
 export var fieldName = ""
 
 func _ready():
-	$Ability.text = fieldName + ": "
+	$AbilityNameLabel.text = fieldName + ": "
 
 func _on_Decrease_pressed():
 	emit_signal("decrease_pressed")
@@ -14,5 +14,6 @@ func _on_Decrease_pressed():
 func _on_Increase_pressed():
 	emit_signal("increase_pressed")
 
-func set_value(value):
+func set_value(value: int):
 	$ProgressBar.value = value
+	$CurrentLevelLabel.text = str(value)
