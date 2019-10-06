4c8a794a0fe0414cff2817b603caef77aab5b81b
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-10-06 10:18:42 -0400

    Add default theme

--- Scripts/WeaponSelect.gd
+++ Scripts/WeaponSelect.gd
@@ -2,9 +2,9 @@ extends PanelContainer
 
 var weapon_id = 0
 
-var default_color = Color(0.243137, 0.243137, 0.243137)
-var hover_color = Color(0.419608, 0.419608, 0.419608)
-var selected_color = Color(0.184314, 0.180392, 0.180392)
+var default_background = preload("res://Assets/Sprites/uipack-rpg/PNG/panel_beige.png")
+var hover_background = preload("res://Assets/Sprites/uipack-rpg/PNG/panel_beigeLight.png")
+var selected_background = preload("res://Assets/Sprites/uipack-rpg/PNG/panel_brown.png")
 
 var selected: bool = false
 
@@ -16,32 +16,32 @@ func initialize(weapon_id: int, sprite, name: String, damage: int, speed: int):
 	$MarginContainer/Rows/WeaponName.text = name
 	$MarginContainer/Rows/DamageRow/DamageValue.text = str(damage)
 	$MarginContainer/Rows/SpeedRow/SpeedValue.text = str(speed)
-	set_background_color(default_color)
+	set_background_color(default_background)
 
 func unselect():
 	selected = false
-	set_background_color(default_color)
+	set_background_color(default_background)
 
-func set_background_color(color: Color):
+func set_background_color(background: Resource):
 	# probably not the ideal way to do this but it works
 	# we need to create different styles since all of the weapon selects
 	# use the same underlying resource, so changing the color on one
 	# changes the color on all of them
 	var style = get("custom_styles/panel").duplicate()
-	style.set("bg_color", color)
+	style.set("texture", background)
 	set("custom_styles/panel", style)
 
 func _on_mouse_entered() -> void:
 	if !selected:
-		set_background_color(hover_color)
+		set_background_color(hover_background)
 
 func _on_mouse_exited() -> void:
 	if !selected:
-		set_background_color(default_color)
+		set_background_color(default_background)
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			selected = true
-			set_background_color(selected_color)
+			set_background_color(selected_background)
 			emit_signal("weapon_selected", weapon_id)
