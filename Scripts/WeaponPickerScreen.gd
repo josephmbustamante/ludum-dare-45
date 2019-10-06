749fe67c72150fcc0ff624ac258f6f999b46f470
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-10-05 22:31:54 -0400

    Add hovering and selected highlighting to weapon select

--- Scripts/WeaponPickerScreen.gd
+++ Scripts/WeaponPickerScreen.gd
@@ -1,8 +1,11 @@
 extends MarginContainer
 
 var weapon_select = preload("res://Scenes/WeaponSelect.tscn")
+var selects: Dictionary
+var selected_weapon_id
 
 onready var startButton = $Rows/CenterContainer/Button
+
 
 func _ready():
 	for weapon in Global.WEAPON:
@@ -18,6 +21,10 @@ func _ready():
 	if (PlayerVariables.deathCount > 0):
 		$Rows/DeathCountText.text = str("YOU HAVE DIED %s TIMES", PlayerVariables.deathCount)
 
+func _process(delta: float) -> void:
+	# don't allow starting the game if we have no selected weapon
+	startButton.disabled = selected_weapon_id == null
+
 func create_weapon_select(weapon_id: int, sprite, name: String, damage: int, speed: int):
 	# create a weapon select for each weapon and add it to the weapon select section
 	var select = weapon_select.instance()
@@ -25,6 +32,7 @@ func create_weapon_select(weapon_id: int, sprite, name: String, damage: int, spe
 	select.initialize(weapon_id, sprite, name, damage, speed)
 	# send a message that the weapon has been selected
 	select.connect("weapon_selected", self, "weapon_menu_selection_made")
+	selects[weapon_id] = select
 
 func _on_Start_Button_pressed():
 	print("Button pressed")
@@ -32,5 +40,12 @@ func _on_Start_Button_pressed():
 
 func weapon_menu_selection_made(id):
 	print(id)
+	# unselect the current weapon (change the color) if one is selected
+	if selected_weapon_id != null:
+		print("unselecting", selected_weapon_id)
+		print(selects[selected_weapon_id])
+		selects[selected_weapon_id].unselect()
+
+	# select the new weapon
+	selected_weapon_id = id
 	PlayerVariables.weapon = id
-	startButton.disabled = false
--- Scripts/WeaponSelect.gd
+++ Scripts/WeaponSelect.gd
@@ -1,18 +1,47 @@
-extends VBoxContainer
+extends PanelContainer
 
 var weapon_id = 0
+
+var default_color = Color(0.243137, 0.243137, 0.243137)
+var hover_color = Color(0.419608, 0.419608, 0.419608)
+var selected_color = Color(0.184314, 0.180392, 0.180392)
+
+var selected: bool = false
 
 signal weapon_selected(weapon_id)
 
 func initialize(weapon_id: int, sprite, name: String, damage: int, speed: int):
 	self.weapon_id = weapon_id
-	$WeaponSprite.texture = sprite
-	$WeaponName.text = name
-	$DamageRow/DamageValue.text = str(damage)
-	$SpeedRow/SpeedValue.text = str(speed)
+	$MarginContainer/Rows/WeaponSprite.texture = sprite
+	$MarginContainer/Rows/WeaponName.text = name
+	$MarginContainer/Rows/DamageRow/DamageValue.text = str(damage)
+	$MarginContainer/Rows/SpeedRow/SpeedValue.text = str(speed)
+	set_background_color(default_color)
 
-func _gui_input(event):
+func unselect():
+	selected = false
+	set_background_color(default_color)
+
+func set_background_color(color: Color):
+	# probably not the ideal way to do this but it works
+	# we need to create different styles since all of the weapon selects
+	# use the same underlying resource, so changing the color on one
+	# changes the color on all of them
+	var style = get("custom_styles/panel").duplicate()
+	style.set("bg_color", color)
+	set("custom_styles/panel", style)
+
+func _on_mouse_entered() -> void:
+	if !selected:
+		set_background_color(hover_color)
+
+func _on_mouse_exited() -> void:
+	if !selected:
+		set_background_color(default_color)
+
+func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
+			selected = true
+			set_background_color(selected_color)
 			emit_signal("weapon_selected", weapon_id)
-
